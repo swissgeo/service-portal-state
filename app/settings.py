1aaa7af78edb2d2e4a77d0f76e9a1a69c4ff8a05
Brice Schaffner <67745584+ltshb@users.noreply.github.com>
2026-04-09 15:39:37 +0200

    Update app/settings.py
    Co-authored-by: Alexander Steiner <alexander.steiner@swisstopo.ch>

--- app/settings.py
+++ app/settings.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     cors_origins: list[str] = Field(default=["*"], description="List of allowed origins in CORS")
     cors_method: list[str] = Field(default=["*"], description="List of allowed methods in CORS")
     cors_headers: list[str] = Field(default=["*"], description="List of allowed headers in CORS")
-    cors_max_age: int = Field(default=600, description="CORS max age")
+    cors_max_age: PositiveInt = Field(default=600, description="CORS max age")
 
 
 settings = Settings()
