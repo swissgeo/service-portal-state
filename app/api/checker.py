d984b22a14e2c38f794012d43ad91671c9227808
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2026-06-15 16:05:06 +0200

    Don't expose the checker EP in the openapi spec
    The ckecker endpoint is only for k8s, so it shouldn't be exposed

--- app/api/checker.py
+++ app/api/checker.py
@@ -6,7 +6,7 @@ from app.version import __version__
 router = APIRouter()
 
 
-@router.get("/checker", summary="Kubernetes Probe", tags=["Internal"])
+@router.get("/checker", summary="Kubernetes Probe", tags=["Internal"], include_in_schema=False)
 async def get_checker() -> Checker:
     """Simple checker endpoint to be used by kubernetes probes"""
     return Checker(success=True, message="OK", version=__version__)
